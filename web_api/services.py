6ca76c5c2c46798df78e5a70517f597a85c27ec7
haarenas <haarenas@uc.cl>
2020-04-05 22:47:19 -0400

    update url base api

--- web_api/services.py
+++ web_api/services.py
@@ -9,16 +9,16 @@ def generate_request(url, params={}):
 
 def get_episodes(ep_id,params={}):
     if ep_id:
-        response = generate_request(f"https://rickandmortyapi.com/api/episode/{ep_id}", params)
+        response = generate_request(f"https://integracion-rick-morty-api.herokuapp.com/api/episode/{ep_id}", params)
     else:
-        response = generate_request('https://rickandmortyapi.com/api/episode/', params)
+        response = generate_request('https://integracion-rick-morty-api.herokuapp.com/api/episode/', params)
     if response:
         if (ep_id and len(ep_id) == 1):
             for ep in range(len(response)):
                 list_pj = []
                 for pj in response[ep]['characters']:
                     list_pj.append(int(pj.split('/')[-1]))
-                response_pjs = requests.get(f"https://rickandmortyapi.com/api/character/{list_pj}").json()
+                response_pjs = requests.get(f"https://integracion-rick-morty-api.herokuapp.com/api/character/{list_pj}").json()
                 response[ep]['personajes'] = response_pjs
             episodes = response
         elif ep_id:
@@ -31,20 +31,20 @@ def get_episodes(ep_id,params={}):
 
 def get_characters(pj_id, params={}):
     if pj_id:
-        response = generate_request(f'https://rickandmortyapi.com/api/character/{pj_id}', params)
+        response = generate_request(f'https://integracion-rick-morty-api.herokuapp.com/api/character/{pj_id}', params)
     else:
-        response = generate_request('https://rickandmortyapi.com/api/character/', params)
+        response = generate_request('https://integracion-rick-morty-api.herokuapp.com/api/character/', params)
     if response:
         if pj_id and len(pj_id) == 1:
             list_ep = []
             for ep in response[0]['episode']:
                 list_ep.append(int(ep.split('/')[-1]))
-            response_ep = requests.get(f"https://rickandmortyapi.com/api/episode/{list_ep}").json()
+            response_ep = requests.get(f"https://integracion-rick-morty-api.herokuapp.com/api/episode/{list_ep}").json()
             response[0]['episodios'] = response_ep
 
             if (response[0]['origin']['url'] != ""):
                 origen_id = int(response[0]['origin']['url'].split('/')[-1])
-                res_origen = requests.get(f"https://rickandmortyapi.com/api/location/{[origen_id]}").json()
+                res_origen = requests.get(f"https://integracion-rick-morty-api.herokuapp.com/api/location/{[origen_id]}").json()
                 response[0]['origen'] = res_origen[0]
             else:
                 response[0]['origen'] = {'id': 0, 'name': 'Desconocido'}
@@ -52,7 +52,7 @@ def get_characters(pj_id, params={}):
 
             if (response[0]['location']['url'] != ""):
                 lugar_actual_id = int(response[0]['location']['url'].split('/')[-1])
-                res_actual = requests.get(f"https://rickandmortyapi.com/api/location/{[lugar_actual_id]}").json()
+                res_actual = requests.get(f"https://integracion-rick-morty-api.herokuapp.com/api/location/{[lugar_actual_id]}").json()
                 response[0]['lugar'] = res_actual[0]
             else:
                 response[0]['lugar'] = {'id': 0, 'name': 'Desconocido'}
@@ -68,15 +68,15 @@ def get_characters(pj_id, params={}):
 
 def get_locations(loc_id, params={}):
     if loc_id:
-        response = generate_request(f'https://rickandmortyapi.com/api/location/{loc_id}', params)
+        response = generate_request(f'https://integracion-rick-morty-api.herokuapp.com/api/location/{loc_id}', params)
     else:
-        response = generate_request('https://rickandmortyapi.com/api/location/', params)
+        response = generate_request('https://integracion-rick-morty-api.herokuapp.com/api/location/', params)
     if response:
         if loc_id and len(loc_id) == 1:
             list_pj = []
             for pj in response[0]['residents']:
                 list_pj.append(int(pj.split('/')[-1]))
-            response_pjs = requests.get(f"https://rickandmortyapi.com/api/character/{list_pj}").json()
+            response_pjs = requests.get(f"https://integracion-rick-morty-api.herokuapp.com/api/character/{list_pj}").json()
             response[0]['residentes'] = response_pjs
             locations = response
         elif loc_id:
@@ -88,9 +88,9 @@ def get_locations(loc_id, params={}):
     return ''
 
 def get_info(name, params={}):
-    response_pjs = generate_request(f'https://rickandmortyapi.com/api/character/?name={name}', params)
-    response_loc = generate_request(f'https://rickandmortyapi.com/api/location/?name={name}', params)
-    response_eps = generate_request(f"https://rickandmortyapi.com/api/episode/?name={name}", params)
+    response_pjs = generate_request(f'https://integracion-rick-morty-api.herokuapp.com/api/character/?name={name}', params)
+    response_loc = generate_request(f'https://integracion-rick-morty-api.herokuapp.com/api/location/?name={name}', params)
+    response_eps = generate_request(f"https://integracion-rick-morty-api.herokuapp.com/api/episode/?name={name}", params)
 
     paginas_pjs = []
     paginas_loc = []
